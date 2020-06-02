683b96e0e85114a45c83641f3b502b0acafa4eb2
Matthew Knight <mgk1795@gmail.com>
2020-06-02 08:58:41 -0700

    fixed indexing

--- exports.zig
+++ exports.zig
@@ -3,7 +3,7 @@ const Allocator = @import("std").mem.Allocator;
 pub fn concat(allocator: *Allocator, lhs: []const u8, rhs: []const u8) ![]const u8 {
     var buf = try allocator.alloc(u8, lhs.len + rhs.len);
 
-    for (buf) |i|
+    for (buf) |_, i|
         buf[i] = if (i < lhs.len) lhs[i] else rhs[i - lhs.len];
 
     return buf;
