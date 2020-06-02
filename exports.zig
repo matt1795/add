49d491b186a6feddec5f3461b9d8a99a50e4fbe2
Matthew Knight <mgk1795@gmail.com>
2020-06-02 08:19:41 -0700

    changed function

--- exports.zig
+++ exports.zig
@@ -1,3 +1,10 @@
-pub fn add(lhs: i32, rhs: i32) i32 {
-    return lhs + rhs;
+const Allocator = @import("std").mem.Allocator;
+
+pub fn concat(allocator: *Allocator, lhs: []const u8, rhs: []const u8) ![]const u8 {
+    var buf = try allocator.alloc(u8, lhs.len + rhs.len);
+
+    for (buf) |i|
+        buf[i] = if (i < lhs.len) lhs[i] else rhs[i - lhs.len];
+
+    return buf;
 }
